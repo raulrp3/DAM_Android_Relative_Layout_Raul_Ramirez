39da205865d93cfe6f4524f68ad97b2f5d83a198
raulrp3 <raul.ramirez@escuelaestech.es>
2019-01-23 19:51:31 +0100

    second relative layout

--- app/src/main/java/com/example/rrp00/relative_layout/MainActivity.java
+++ app/src/main/java/com/example/rrp00/relative_layout/MainActivity.java
@@ -8,6 +8,7 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.relative_layout_01);
+        //setContentView(R.layout.relative_layout_01);
+        setContentView(R.layout.relative_layout_02);
     }
 }
