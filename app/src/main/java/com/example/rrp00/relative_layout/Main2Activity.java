0d16653c21fb9cba878c74c7d0b2ee075579fc6e
Raul Ramirez <raul.ramirez@escuelaestech.es>
2019-01-30 12:48:57 +0100

    add intent

--- app/src/main/java/com/example/rrp00/relative_layout/Main2Activity.java
+++ app/src/main/java/com/example/rrp00/relative_layout/Main2Activity.java
@@ -1,22 +1,20 @@
 package com.example.rrp00.relative_layout;
 
 import android.content.Intent;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
 
-public class MainActivity extends AppCompatActivity {
-
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
+public class Main2Activity extends AppCompatActivity {
+    protected void onCreate(Bundle savedInstanceState){
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.relative_layout_01);
-        Button button = findViewById(R.id.btnRLayout02);
+        setContentView(R.layout.relative_layout_02);
+        Button button = findViewById(R.id.btnRLayout01);
         button.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent intent = new Intent(MainActivity.this,Main2Activity.class);
+                Intent intent = new Intent(Main2Activity.this,MainActivity.class);
                 startActivity(intent);
             }
         });
